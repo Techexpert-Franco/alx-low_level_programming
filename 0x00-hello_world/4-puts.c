922a08bb1b9f9950909a4435749a9b48ca92f041
Techexpert-Franco <francisstephen70@gmail.com>
2022-09-09 19:24:40 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
-#!/bin/bash
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main-Entry point
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * puts message
+ * Return: 0
  */
 int main(void)
 {
