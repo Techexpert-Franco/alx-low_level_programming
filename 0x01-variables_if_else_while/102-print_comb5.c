82a1bf98af8e83a9dea56846162cb4a41a29119a
Techexpert-Franco <francisstephen70@gmail.com>
2022-09-09 18:22:05 -0700

    printint combo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,8 +14,10 @@ int main(void)
 				if (i != j)
 				{
 					putchar(j / 10 + 48);
-					putchar(j % 10 + 48);								putchar(' ');									putchar(i / 10 + 48);
-					putchar(i % 10 + 48);			
+					putchar(j % 10 + 48);
+					putchar(' ');
+					putchar(i / 10 + 48);
+					putchar(i % 10 + 48);
 					if (j * 100 + i != 9899)	
 					{
 						putchar(',');
