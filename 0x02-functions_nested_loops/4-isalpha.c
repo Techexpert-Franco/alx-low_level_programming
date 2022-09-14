fdab24de05331aecc78e64d700285de132081821
Techexpert-Franco <francisstephen70@gmail.com>
2022-09-13 18:26:01 -0700

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,12 +6,12 @@
  */
 int _isalpha(int c)
 {
-       	if ((c >= 97 && c <= 123) || (c >= 65 && c <= 91))
+	if ((c >= 97 && c <= 123) || (c >= 65 && c <= 91))
 	{
 		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
