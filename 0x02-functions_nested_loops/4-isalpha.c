0f3fd2d1e6aa9426a706632c9d55afd856cf110f
Techexpert-Franco <francisstephen70@gmail.com>
2022-09-13 17:22:00 -0700

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,12 @@
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
-
+{	if ((c>=97 && c<=123) || (c>=65 && c<=91));
 {
-		return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+		return (1);
+}
+	else
+{
+	return(0);
+}
 }
