cb151b17c24686cf43441c275284a7478ec2d49c
Techexpert-Franco <francisstephen70@gmail.com>
2022-09-14 18:18:08 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,15 +6,15 @@
  */
 void print_times_table(int n)
 {
-	int x = 0,y,z;
+	int x = 0, y, z;
 
 	if (n > 15 || n < 0)
 		return;
-	while (x <=n)
+	while (x <=n )
 	{
 		for (y = 0; y <= n; y++)
 		{
-			z = x*y;
+			z = x * y;
 			if (z > 99)
 			{
 				_putchar(z / 100 + '0');
@@ -27,7 +27,7 @@ void print_times_table(int n)
 				_putchar(z / 10 + '0');
 				_putchar(z % 10 + '0');
 			}
-			else if (y !=0)
+			else if (y ! = 0)
 			{
 				_putchar('');
 				_putchar('');
@@ -35,7 +35,7 @@ void print_times_table(int n)
 			}
 			else
 				_putchar(z + '0');
-			if (y !=n)
+			if (y ! = n)
 			{
 				_putchar('');
 				_putchar('');
