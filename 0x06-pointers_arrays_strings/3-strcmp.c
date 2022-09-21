c7ec4dbf4a733cc3a21a765021073f184608a794
Techexpert-Franco <francisstephen70@gmail.com>
2022-09-21 16:40:07 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,10 +12,10 @@ int _strcmp(char *s1, char *s2)
 		int counter, cmpVal;
 
 			counter = 0;
-				while (s1[counter] == s2[counter] && s1[counter] != '\0')
-						{
-									counter++;
-										}
+			while (s1[counter] == s2[counter] && s1[counter] != '\0')
+				{
+					counter++;
+				}
 					cmpVal = s1[counter] - s2[counter];
 						return (cmpVal);
 }
