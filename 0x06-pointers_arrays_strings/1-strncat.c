b31d1a6bc98bbf4e101a957cf82823817f633fca
Techexpert-Franco <francisstephen70@gmail.com>
2022-09-21 16:14:48 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,14 +14,14 @@ char *_strncat(char *dest, char *src, int n)
 		/* j is a counter for  n bytes of src to be concatenated */
 		/* length = length of destination string */
 			length = 0;
-				while (dest[length] != '\0')
-						{
-									length++;
-										}
-					for (j = 0; j < n && src[j] != '\0'; j++, length++)
-							{
-										dest[length] = src[j];
-											}
-						dest[length] = '\0';
-							return (dest);
+			while (dest[length] != '\0')
+			{	
+				length++;
+			}
+				for (j = 0; j < n && src[j] != '\0'; j++, length++)
+				{
+					dest[length] = src[j];
+				}
+					dest[length] = '\0';
+						return (dest);
 }
