24ecef4703e49e6396f563359af258320d4a424c
Techexpert-Franco <francisstephen70@gmail.com>
2022-09-22 17:09:27 -0700

    magis

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,10 +5,10 @@ int main(void)
 	  int a[5];
 	  int *p;
 
-	        a[2] = 1024;
-		  p = &n;
-        /*
-	* write your line of code here...
+		a[2] = 1024;
+		p = &n;
+        /** 
+	*write your line of code here...
 	* Remember:
 	* - you are not allowed to use a
 	* - you are not allowed to modify p
@@ -18,5 +18,5 @@ int main(void)
 		    *(p + 5) = 98;
 		      /* ...so that this prints 98\n */
 		      printf("a[2] = %d\n", a[2]);
-		        return (0);
+		      return (0);
 }
