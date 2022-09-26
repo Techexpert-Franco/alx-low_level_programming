fc98c86bed9fece073d82c8d7aa1a98cec575993
Techexpert-Franco <francisstephen70@gmail.com>
2022-09-26 07:34:42 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 		unsigned int i;
 
 			i = 0;
-			while (i < n)  /*Delacring WHILE*/
+	while (i < n)  /*Delacring WHILE*/
 		{
 		*(dest + i) = *(src + i); /*add 1 position dest and src*/
 		i++;
