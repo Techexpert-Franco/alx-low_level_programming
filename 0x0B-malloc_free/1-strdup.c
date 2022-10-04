e596b40cdab45f93bd2e20346ec123fbb6519392
Techexpert-Franco <francisstephen70@gmail.com>
2022-10-04 12:17:38 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,9 +16,9 @@ char *_strdup(char *str)
 		for (i = 0; str[i] != '\0'; i++)
 			;
 		strout = (char *)malloc(sizeof(char) * (i + 1));
-	if (strout == NULL)
-			return (NULL);
-	for (j = 0; j <= i; j++)
+		if (strout == NULL)
+		return (NULL);
+		for (j = 0; j <= i; j++)
 			strout[j] = str[j];
 	return (strout);
 }
